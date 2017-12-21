d7ed9b79e88dbda0433c84f5240065c21eeed65d
monkey-master <admin@rhel.cc>
2017-12-21 16:36:28 +0800

    去除离线缓存,但是感觉和bug关系不大

--- assets/src/main.js
+++ assets/src/main.js
@@ -18,52 +18,52 @@ require('@/assets/admin/js/inspinia.js')
 require('@/assets/admin/js/plugins/pace/pace.min.js')
 
 
-// Vue.config.productionTip = true
+Vue.config.productionTip = true
 
 const notyDefault = {
-  type: 'info',
-  layout: 'bottomRight',
-  timeout: 1000,
-  progressBar: true
+    type: 'info',
+    layout: 'bottomRight',
+    timeout: 1000,
+    progressBar: true
 }
 
-Vue.prototype.$noty = function (opts) {
-  new Noty(Object.assign({}, notyDefault, opts)).show()
+Vue.prototype.$noty = function(opts) {
+    new Noty(Object.assign({}, notyDefault, opts)).show()
 }
 
-Vue.prototype.$showSuccess = function (message) {
-  new Noty(Object.assign({}, notyDefault, {
-    text: message,
-    type: 'success'
-  })).show()
+Vue.prototype.$showSuccess = function(message) {
+    new Noty(Object.assign({}, notyDefault, {
+        text: message,
+        type: 'success'
+    })).show()
 }
 
-Vue.prototype.$showError = function (error) {
-  let n = new Noty(Object.assign({}, notyDefault, {
-    text: error,
-    type: 'error',
-    timeout: null,
-    buttons: [
-      Noty.button(i18n.t('buttons.reportIssue'), '', function () {
-        window.open('https://github.com/hacdias/filemanager/issues/new')
-      }),
-      Noty.button(i18n.t('buttons.close'), '', function () {
-        n.close()
-      })
-    ]
-  }))
+Vue.prototype.$showError = function(error) {
+    let n = new Noty(Object.assign({}, notyDefault, {
+        text: error,
+        type: 'error',
+        timeout: null,
+        buttons: [
+            Noty.button(i18n.t('buttons.reportIssue'), '', function() {
+                window.open('https://github.com/hacdias/filemanager/issues/new')
+            }),
+            Noty.button(i18n.t('buttons.close'), '', function() {
+                n.close()
+            })
+        ]
+    }))
 
-  n.show()
+    n.show()
 }
 
-Vue.config.devtools = true
+// Vue.config.devtools = true
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  store,
-  router,
-  i18n,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    store,
+    router,
+    i18n,
+    template: '<App/>',
+    components: { App }
 })
--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ fi
 # Clean the dist folder and build the assets
 rm -rf assets/dist
 npm run build
+rm -f assets/dist/sw.js
 mkdir -p assets/dist/filemanager 
 mv assets/dist/static assets/dist/filemanager
 # Embed the assets using rice
